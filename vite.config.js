69f4921b2dd90b2082f02afade70ce040e376c1f
Joohyun Park <jhp527@nyu.edu>
2024-08-24 21:29:34 -0400

    update package

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     port: 3000,
+    open: true,
   },
   // https://vitejs.dev/config/shared-options.html#base
   base: "/github-page/",
